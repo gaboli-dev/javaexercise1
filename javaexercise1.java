d09b33265858439e46261554fe3af4851f0aca3e
Gabriel Oliveira <gabrielsil.o19@hotmail.com>
2026-03-17 01:07:26 -0300

    Create javaexercise1.java
    My first exercise!!

---
+++ javaexercise1.java
@@ -0,0 +1,35 @@
+package javaexercises;
+
+import java.util.Locale;
+
+public class javaexercise1 {
+
+	public static void main(String[] args) {
+		
+		
+		String product1 = "Computador";
+		String product2 = "Office desk";
+		
+		int idade = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.00;
+		double price2 = 650.50;
+		double medida = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		System.out.println();
+	    System.out.printf("Record: %d years old, code %d and gender: %c%n" , idade, code, gender);
+	    System.out.println();
+	    System.out.printf("Measue with eight decimal places: %.8f%n", medida);
+	    System.out.printf("Rouded (three decimal places): %.3f%n", medida);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f%n", medida);
+	    
+
+	}
+	
+}
